4fb06b1a3b377c4c6482a7e88b004b050d868840
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-08-07 20:37:13 +0530

    combinotorics added

---
+++ Learning stuffs/number_Theory/Combinotorics/Permutations.cpp
@@ -0,0 +1,13 @@
+
+// nPr where n is number of items and r is how many of them are selected,
+// in programing generally all of them are selected so generally r = n.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // number of permutations of this array
+    int n = 3;
+    int Arr[n];
+    // factorial of n
+}
