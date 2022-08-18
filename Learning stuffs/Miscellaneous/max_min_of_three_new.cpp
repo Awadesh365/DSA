c1e0e5986dc019a220326f506a7feea29180075a
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-08-18 11:57:17 +0530

    nesting min max without nesting update

--- Learning stuffs/Miscellaneous/max_min_of_three_new.cpp
+++ Learning stuffs/Miscellaneous/max_min_of_three_new.cpp
@@ -5,4 +5,5 @@ int main()
 {
     // without nesting we can find maximum of three and so on.
     cout << max({0, 1, 2}) << endl; // max(max(counter[0],counter[1]),couner[2])
+    cout << min({3, 32, 4423, 23}) << endl;
 }
