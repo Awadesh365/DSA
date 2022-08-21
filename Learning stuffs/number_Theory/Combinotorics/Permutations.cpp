77216fa4372f4a45a7d681f1989fa895e6f14db3
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-08-21 16:40:59 +0530

    next permutation

--- Learning stuffs/number_Theory/Combinotorics/Permutations.cpp
+++ Learning stuffs/number_Theory/Combinotorics/Permutations.cpp
@@ -3,11 +3,16 @@
 // in programing generally all of them are selected so generally r = n.
 #include <bits/stdc++.h>
 using namespace std;
+// #define possibilities(x) while (next_permutation(x.begin(), x.end()))
 
 int main()
 {
     // number of permutations of this array
     int n = 3;
     int Arr[n];
+
+    vector<int> a = {23, 32, 434, 54, 46, 65, 675};
+    cout << next_permutation(a.begin(), a.end());
+
     // factorial of n
 }
