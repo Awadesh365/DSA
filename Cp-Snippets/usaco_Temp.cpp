1f918d65c13d7c3a48e1689aa4fe38dadd57f86d
Awadesh <76896819+Awadesh365@users.noreply.github.com>
2022-09-02 11:54:42 +0530

    Update usaco_Temp.cpp

--- Cp-Snippets/usaco_Temp.cpp
+++ Cp-Snippets/usaco_Temp.cpp
@@ -1,5 +1,11 @@
-// input output for the submission in usaco problmes
+// Before submitting solutions in usaco problems you should add following in your code.
 
+
+#include <cstdio> //  <-  1 This Header with your existing Header
+
+
+
+// 2 ->  input output for the submission in usaco problmes  
 void setIO(string name = "")
 { // FastIO see General -> Fast Input and Output
     ios_base::sync_with_stdio(0);
@@ -13,8 +19,10 @@ void setIO(string name = "")
 
 int main()
 {
-    setIO("");
-
+     setIO("");    /* <- 3 -> This funtion call, add the name of input/output inside those quotation marks, let's say your name of
+     input and output of your problem is ThreeBalls.in and  ThreeBalls.out then you should add ThreeBalls inside Those quotation marks.
+     eg setIO("ThreeBalls");
+*/
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
