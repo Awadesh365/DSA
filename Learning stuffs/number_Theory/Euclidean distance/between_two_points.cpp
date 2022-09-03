f206859a1c870c2385a89ad2381ec7196140c8c6
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-09-03 11:20:09 +0530

    // Euclidean distance between two points

---
+++ Learning stuffs/number_Theory/Euclidean distance/between_two_points.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// distance[(x1​,y1​),(x2​,y2​)]^2=(x2​−x1​)^2+(y2​−y1​)^2
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+
+    for (auto &it : a)
+        cin >> it;
+
+    for (auto &it : b)
+        cin >> it;
+
+    int ans = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int x = a[i] - a[j], y = b[i] - b[j];
+            int distance = (x * x + y * y);
+
+            ans = max(ans, distance);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
