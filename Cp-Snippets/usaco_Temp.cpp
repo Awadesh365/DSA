d527555dfe5b0339bb6fc6df3e8cd7aa955b8c31
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-09-18 13:47:35 +0530

    usaco template update

--- Cp-Snippets/usaco_Temp.cpp
+++ Cp-Snippets/usaco_Temp.cpp
@@ -1,11 +1,99 @@
-// Before submitting solutions in usaco problems you should add following in your code.
+/*  Awadesh Nautiyal  */
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
 
-#include <cstdio> //  <-  1 This Header with your existing Header
+// For debugging
+/*-------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
 
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
 
-
-// 2 ->  input output for the submission in usaco problmes  
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+// END of Debugging
+/*--------------------------------------------*/
 void setIO(string name = "")
 { // FastIO see General -> Fast Input and Output
     ios_base::sync_with_stdio(0);
@@ -17,12 +105,26 @@ void setIO(string name = "")
     }
 }
 
+const ll N = 1e6;
+void precompute()
+{
+}
+void solve()
+{
+}
 int main()
 {
-     setIO("");    /* <- 3 -> This funtion call, add the name of input/output inside those quotation marks, let's say your name of
-     input and output of your problem is ThreeBalls.in and  ThreeBalls.out then you should add ThreeBalls inside Those quotation marks.
-     eg setIO("ThreeBalls");
-*/
+    // setIO("");
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // precompute();
+
+    // ll TESTS;
+    // cin >> TESTS;
+    // while (TESTS--)
+    solve();
 
     return 0;
 }
