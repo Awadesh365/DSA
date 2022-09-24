912e2d87ed626bacefec704b5b40d21b44c15438
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-09-24 23:21:00 +0530

    app div

---
+++ CSES/Introductory Problems/Apple_Division.cpp
@@ -0,0 +1,135 @@
+/*  Awadesh Nautiyal  */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+// For debugging
+/*-------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+// END of Debugging
+/*--------------------------------------------*/
+vector<ll> a;
+ll n;
+
+ll ans(ll i, ll s1, ll s2)
+{
+    if (i == n)
+        return abs(s1 - s2);
+    else
+    {
+        ll check1 = ans(i + 1, s1, s2 + a[i]), check2 = ans(i + 1, s1 + a[i], s2);
+        return min(check1, check2);
+    }
+}
+
+void solve()
+{
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        a.push_back(x);
+    }
+    cout << ans(0, 0, 0) << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // precompute();
+
+    // ll TESTS;
+    // cin >> TESTS;
+    // while (TESTS--)
+    solve();
+
+    return 0;
+}
