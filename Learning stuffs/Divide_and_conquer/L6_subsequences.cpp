e23dfc6bb64534b063c0e3bfcfa60dc7b8157796
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-09-26 21:03:21 +0530

    take/ not take conditions

--- Learning stuffs/Divide_and_conquer/L6_subsequences.cpp
+++ Learning stuffs/Divide_and_conquer/L6_subsequences.cpp
@@ -8,18 +8,21 @@ void generate_subsequences(int ind, vector<int> &ds, vector<int> arr, int n)
         for (auto &it : ds)
             cout << it << " ";
 
+        // for printing the Empty vector at last else it will simply prints black sapces which is also correct
         if (ds.size() == 0)
             cout << "{}";
 
         cout << endl;
         return;
     }
-
-    generate_subsequences(ind + 1, ds, arr, n);
-
+    // take or pick the particular index into the subsequence
     ds.push_back(arr[ind]);
     generate_subsequences(ind + 1, ds, arr, n);
     ds.pop_back();
+
+    // not pick, or not take condition, this element is not added to your subsequence
+
+    generate_subsequences(ind + 1, ds, arr, n);
 }
 int main()
 {
