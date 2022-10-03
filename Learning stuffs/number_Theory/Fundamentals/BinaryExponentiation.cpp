2f24d09d73c57f906b94a6d5167a0eff2d4beace
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-10-03 14:29:55 +0530

    binpow upd

--- Learning stuffs/number_Theory/Fundamentals/BinaryExponentiation.cpp
+++ Learning stuffs/number_Theory/Fundamentals/BinaryExponentiation.cpp
@@ -1,11 +1,14 @@
-ll modpow(ll b, ll e)
-{
-    ll ans = 1;
-    /*
+
+/*
      In This case, we don't have a, so This loop does nothing at the start.
      then c is (e >>= 1) which is short for (e = e >> 1), right shift e by one bit.
       It is basically divining e by 2 at every loop (throwing out remainders)
     */
+
+ll modpow(ll b, ll e)
+{
+    ll ans = 1;
+
     for (;; e >>= 1) // or for (;; e /= 2) or  for(;e>0;e>>=1) or for(;;e= e >> 1)
     {
         if (e <= 0)
