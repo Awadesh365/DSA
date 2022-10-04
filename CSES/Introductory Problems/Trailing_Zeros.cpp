ab09e51c68e17a5ae79fc37993dfc67e58b629c0
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-10-05 00:06:28 +0530

    cses upt ni pal_re

---
+++ CSES/Introductory Problems/Trailing_Zeros.cpp
@@ -0,0 +1,137 @@
+/*  Awadesh Nautiyal  */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+// For debugging
+/*-------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+// END of Debugging
+/*--------------------------------------------*/
+const ll N = 1e6;
+void precompute()
+{
+}
+// ----------------------------------------------------
+// standard problem ref->https://www.geeksforgeeks.org/count-trailing-zeroes-factorial-number/
+// Function to return trailing
+// 0s in factorial of n
+
+ll Find_Lastzeros(ll n)
+{
+    if (n < 0)
+        return -1;
+
+    int cnt = 0;
+    for (ll i = 5; (n / i) >= 1; i *= 5)
+        cnt += (n / i);
+
+    return cnt;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    cout << Find_Lastzeros(n) << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // precompute();
+
+    // ll TESTS;
+    // cin >> TESTS;
+    // while (TESTS--)
+    solve();
+
+    return 0;
+}
