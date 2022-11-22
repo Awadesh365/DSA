ec6b5b894c85df402559b9456898c0aec39d9920
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-11-22 21:01:25 +0530

    p[r]-p[l-1]

---
+++ Learning stuffs/Precomputation/USACO/sum_of_values_of_subaray.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+we are given the prefix sum array p=[2,7,14,15,19].
+we need to find out the sum of values of the original array for the ranges of indexes [1,4] and [2,5](inclusive,1-indexed)
+
+*/
+// sum of values of some subarray [l,r] is determined by the
+// formula p[r] - p[l-1]
+
+int main()
+{
+    return 0;
+}
