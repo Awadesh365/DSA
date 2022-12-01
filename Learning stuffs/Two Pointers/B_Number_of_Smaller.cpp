60221aed464085ddd9d3bc36a3cfac3631869d5d
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-12-01 19:55:08 +0530

    A and B cf edu

---
+++ Learning stuffs/Two Pointers/B_Number_of_Smaller.cpp
@@ -0,0 +1,138 @@
+/*  Awadesh Nautiyal  */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+// For debugging
+/*-------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+// END of Debugging
+/*--------------------------------------------*/
+const ll N = 1e5 + 10;
+map<ll, ll> mp;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> a(n), b(m);
+
+    for (auto &it : a)
+        cin >> it;
+
+    for (auto &it : b)
+        cin >> it;
+
+    vector<ll> ans(m);
+    ll start = 0;
+
+    for (ll i = 0; i < m; i++)
+    {
+        while (start < n && a[start] < b[i])
+        {
+            start++;
+        }
+        ans[i] = start;
+    }
+
+    for (auto &it : ans)
+        cout << it << " ";
+
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
