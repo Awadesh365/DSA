96fe7504110cba300efe22081d40f96bfaa1ca2e
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-12-06 19:14:31 +0530

    prob b

---
+++ Learning stuffs/Two Pointers/CF Edu/Step 2/B_Segment_with_Big_Sum.cpp
@@ -0,0 +1,136 @@
+/*  Awadesh Nautiyal  */
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+// For debugging
+/*-------------------------------------------------*/
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(ld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+// END of Debugging
+/*--------------------------------------------*/
+const ll N = 1e6;
+void solve()
+{
+    ll n, s;
+    cin >> n >> s;
+
+    vector<ll> a(n);
+    for (auto &it : a)
+        cin >> it;
+
+    ll mx = 1e18;
+
+    ll sum = 0, l = 0;
+    ll ans = mx;
+    for (ll r = 0; r < a.size(); r++)
+    {
+        sum += a[r];
+        while (sum - a[l] >= s)
+            sum -= a[l], l++;
+
+        if (sum >= s) // we are not considering the sum smaller that s.
+            ans = min(ans, (r - l + 1));
+    }
+    if (ans == mx) // if no such ans exist
+        cout << "-1" << endl;
+    else
+        cout << ans << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    // ll TESTS;
+    // cin >> TESTS;
+    // while (TESTS--)
+    solve();
+
+    return 0;
+}
