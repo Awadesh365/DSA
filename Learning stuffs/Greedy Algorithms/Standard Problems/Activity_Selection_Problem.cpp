8f7ebae5964a306e1a7da0dd3793f52f8919f66c
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2022-12-13 18:50:24 +0530

    activity selecton problem

--- Learning stuffs/Greedy Algorithms/Standard Problems/Activity_Selection_Problem.cpp
+++ Learning stuffs/Greedy Algorithms/Standard Problems/Activity_Selection_Problem.cpp
@@ -3,36 +3,7 @@ https://practice.geeksforgeeks.org/problems/activity-selection-1587115620/1?utm_
 
 */
 
-#include <bits/stdc++.h>static bool pairCompare(const pair<pair<int, int>, int> &a, const pair<pair<int, int>, int> &b)
-{
-    if (a.first.second != b.first.second)
-        return a.first.second < b.first.second;
-
-    return a.second < b.second;
-}
-
-int activitySelection(vector<int> start, vector<int> end, int n)
-{
-    vector<pair<pair<int, int>, int>> x;
-
-    for (int i = 0; i < n; i++)
-        x.push_back({{start[i], end[i]}, i + 1});
-
-    sort(x.begin(), x.end(), pairCompare);
-
-    int last = 0;
-    int ans = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (x[i].first.first > last)
-        {
-            ans++;
-            last = x[i].first.second;
-        }
-    }
-    return ans;
-}
+#include <bits/stdc++.h>
 using namespace std;
 
 static bool pairCompare(const pair<pair<int, int>, int> &a, const pair<pair<int, int>, int> &b)
