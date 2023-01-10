920504c5249945bef35f012742ba14b46623a51f
awadesh.202004081@tulas.edu.in <awadesh.202004081@tulas.edu.in>
2023-01-11 00:16:18 +0530

    substr

--- Learning stuffs/Strings/substr()_function.cpp
+++ Learning stuffs/Strings/substr()_function.cpp
@@ -3,8 +3,16 @@ using namespace std;
 
 int main()
 {
-    string s = "hello";
+    string s = "GoodToSeeYou";
+    // starting from 0 and going to 1, not including the 2nd index.
     string a = s.substr(0, 2);
-    cout << a << endl;
+
+    int x = 3;
+    string b = s.substr(1, x); // starting from 1 index and going till before x, not including x, i.e exclusive.
+
+    cout << a << " " << b << endl;
+    cout << "-----------" << endl;
+    cout << s.substr(x - 3) << endl;
+
     return 0;
 }
